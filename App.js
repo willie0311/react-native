1cc10060620b9af4d08f0610c12b7fcd60f4904d
willie <willie@gmail.com>
2022-07-07 08:31:40 +0800

    初始化

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View,SafeAreaView,TextInput } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>中彰投分屬 跨平台測試 2022/07/01 李宏偉 20號</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view01}>
+        <Text style={styles.text01}>中彰投</Text>
+      </View>
+      <View style = {{height:100 , width:200 , borderWidth:1}}>
+      <Text style={styles.text01}>2022/07/01 跨平台</Text>
+      <TextInput 
+        style = {{height:40 , width:180 , borderWidth:2}} placeholder = "請輸入"
+        onChange={() =>{ console.log('點擊測試')}}  
+      /> 
+      <Button 
+        title='按鍵'
+        onPress = {() =>{console.log('按鈕測試')}}
+      />
+    </View>   
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +30,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view01: {
+    height:100 ,
+    width:100 ,
+    borderWidth:1
+  },
+  text01: {
+    color: 'red',
+  }
 });
