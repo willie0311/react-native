2c82a1d236fa9898ba5ad7ad2cad9adb6eb5cce4
willie <willie@gmail.com>
2022-07-07 12:39:34 +0800

    scrollView 卷軸

--- App.js
+++ App.js
@@ -1,76 +1,21 @@
-import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image  } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView  } from 'react-native';
 
 
 
 
 export default function App() {
   return (
-   <SafeAreaView style={styles.container}>
-   <Image 
-      style = {{height:100, width:100}}
-      source = {require('./assets/123.jpg')}
-      resizeMode = 'contain'
-    />
-      {/*<View style={styles.view01}>
-        <Text style={styles.text01}>中彰投</Text>
+  <SafeAreaView style={styles.container}>
+  <View style ={{width:300 , height:400}}>
+      <ScrollView>
+      <View style = {{backgroundColor:'red',height:300}}>
       </View>
-        <View style = {{height:100 , width:200 , borderWidth:1}}>
-        <Text style={styles.text01}>2022/07/01 跨平台</Text>
-        <TextInput 
-          style = {{height:40 , width:180 , borderWidth:2}} placeholder = "請輸入"
-          onChange={() =>{ console.log('點擊測試')}}  
-      /> 
-
-      
-      <Button
-        title = "press me."
-        onPress= {() => {console.log('onPress')}} 
-      />
-
-      </View>   
-  
-      <TouchableHighlight
-        underlayColor ="gray"
-        activeOpacity = {0.5}
-        onPress = {() =>{console.log('TouchableHighlight')}} >
-        <Text>Button</Text>
-      </TouchableHighlight>
-
-
-      <TouchableOpacity
-        activeOpacity = {0.5}
-        onPress = {() =>{console.log('TouchableOpacity')}} >
-        <Text>Button</Text>
-      </TouchableOpacity>
-      
-      <TouchableNativeFeedback
-      onPress = {() =>{console.log('TouchableNativeFeedback')}}
-      background ={TouchableNativeFeedback.Ripple('gray',true)}
-      hitSlop = {{top:10,left:10,bottom:10,right:10}}>
-      <View style ={{backgroundColor:'blue'}}>
-        <Text style={{margin:30}}>Button</Text>
+      <Text style= {{fontSize:25}}> This is ScrollView</Text>
+      <View style = {{borderWidth:2 ,height:300}}>
       </View>
-    </TouchableNativeFeedback>
-    <Image 
-      style = {{height:100, width:100}}
-      source = {require('./assets/logo.png')}
-    />
-
-    <TouchableOpacity
-    activeOpacity = {0.5}
-    onPress = {() =>{console.log('TouchableOpacity')}} >
-    <View>
-      <Text></Text>
-      <Image 
-      style = {{height:100, width:100}}
-      source = {require('./assets/logo.png')}
-    />
-    </View>
-    
-  </TouchableOpacity>*/}
-    
-
-    </SafeAreaView>
+    </ScrollView>
+  </View>
+  </SafeAreaView>
 
 
   );
