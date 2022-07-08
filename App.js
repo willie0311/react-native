7c2dad5865ae51d8d542d6691371375931184498
willie <willie@gmail.com>
2022-07-08 14:27:45 +0800

    整合組件 Switch 開關

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView , FlatList ,SectionList  } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView , FlatList ,SectionList ,Switch } from 'react-native';
 
 
 //增加npm
@@ -6,41 +6,17 @@ import PropTypes from 'prop-types';
 
 import React , {Component} from 'react';
 
-export default class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      myState : 'My First State',
-      myWorld : 'World',
-    };
-  }
-  // add 觸發事件
-  changeText1(){
-    this.setState({
-      myState : 'willie',
-      myWorld : '9999'
-    })
-  }
-  // add 觸發事件
-  changeText2(){
-    this.setState({
-      myState : 'abby',
-      myWorld : '8888'
-    })
-  }
 
+export default class App extends Component {
+  
   render (){
     return (
       <View style = {styles.container}>
-        <Text> Hello {this.state.myState}</Text>
-        <Text> Hello {this.state.myWorld}</Text>
-        <Button
-          title = "chang1"
-          onPress={() => this.changeText1()}
-        />
-        <Button
-          title = "chang2"
-          onPress={() => this.changeText2()}
+        <Text style = {{fontSize :25}}>Switch </Text>
+        <Switch
+          // disabled = {true} 
+          // value = {this.state.value}
+
         />
       </View>
     );
