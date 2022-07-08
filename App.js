28c4ee9e34a6896ad0f97438fe55175502cc1137
willie <willie@gmail.com>
2022-07-08 10:15:07 +0800

    props屬性 自訂義 Class

--- App.js
+++ App.js
@@ -1,52 +1,22 @@
 import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView , FlatList ,SectionList  } from 'react-native';
 
+import { Component } from "react";
+
+//自訂義hello
+class Hello extends Component {
+  render (){
+    return (
+      <Text>Hello {this.props.title}</Text>
+    )
+  }
+} 
 
 export default function App() {
-  var data01 = [
-    { title: 'Title1', data: ['item1','item2']},
-    { title: 'Title2', data: ['item3','item4']},
-    { title: 'Title3', data: ['item5','item6']},
-  ];
+
   return (
-    <View style={styles.container}>
-      <View style={{width: 300, height: 400}}>
-        <SectionList
-          renderItem={({ item, index, section}) => (
-            <Text key={index}>{item}</Text>
-          )}
-          renderSectionHeader={
-            ({section: {title} }) =>(
-              <View
-                style={{backgroundColor: 'black',
-                marginTop:20
-              }}>
-                <Text style={{ color: "white"}}>
-                {title}
-                </Text>
-              </View>
-            )}
-          sections={data01}
-          keyExtractor={(item, index) => item + index}  
-        />
-        <SectionList
-          renderItem={({ item, index, section}) => (
-            <Text key={index}>{item}</Text>
-          )}
-          renderSectionHeader={
-            ({section: {title} }) =>(
-              <View
-                style={{backgroundColor: 'black',
-                marginTop:20
-              }}>
-                <Text style={{ color: "white"}}>
-                {title}
-                </Text>
-              </View>
-            )}
-          sections={data01}
-          keyExtractor={(item, index) => item + index}  
-        />
-      </View>
+    <View style ={styles.container}>
+      <Hello title='World' />
+      <Hello title='React Native' />
     </View>
   );
 }
