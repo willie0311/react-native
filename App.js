0e7df40214388114019314034168e434381f1ec6
willie <willie@gmail.com>
2022-07-08 14:08:08 +0800

    按鈕切換State 狀態的文字畫面

--- App.js
+++ App.js
@@ -14,11 +14,34 @@ export default class App extends Component {
       myWorld : 'World',
     };
   }
+  // add 觸發事件
+  changeText1(){
+    this.setState({
+      myState : 'willie',
+      myWorld : '9999'
+    })
+  }
+  // add 觸發事件
+  changeText2(){
+    this.setState({
+      myState : 'abby',
+      myWorld : '8888'
+    })
+  }
+
   render (){
     return (
       <View style = {styles.container}>
         <Text> Hello {this.state.myState}</Text>
         <Text> Hello {this.state.myWorld}</Text>
+        <Button
+          title = "chang1"
+          onPress={() => this.changeText1()}
+        />
+        <Button
+          title = "chang2"
+          onPress={() => this.changeText2()}
+        />
       </View>
     );
   }
