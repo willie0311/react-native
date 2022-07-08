a3bd78469e12d1d5e898bff1cbd05520e59a8dbf
willie <willie@gmail.com>
2022-07-08 13:42:53 +0800

    State 狀態

--- App copy.js
+++ App copy.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// add 
+import React , {Component} from 'react';
 
-export default function App() {
+{/*export default function App() {
   return (
     <View style={styles.container}>
       <Text>中彰投分屬 跨平台測試 2022/07/01 李宏偉 20號</Text>
       <StatusBar style="auto" />
     </View>
   );
+}*/}
+
+// add state
+export default class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+      <Text>中彰投分屬 跨平台測試 2022/07/01 李宏偉 20號</Text>
+      <StatusBar style="auto" />
+    </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- App.js
+++ App.js
@@ -1,44 +1,29 @@
 import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView , FlatList ,SectionList  } from 'react-native';
 
-import { Component } from "react";
+
 //增加npm
 import PropTypes from 'prop-types';
 
-//自訂義hello
-class Hello extends Component {
+import React , {Component} from 'react';
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myState : 'My First State',
+      myWorld : 'World',
+    };
+  }
   render (){
     return (
-      // 一個物件不用View 兩個以上需要View包起來 才能回傳
-      <View>
-        <Text>title {this.props.title}</Text>
-        <Text>name {this.props.name}</Text>
-        <Text>id {this.props.id}</Text>
+      <View style = {styles.container}>
+        <Text> Hello {this.state.myState}</Text>
+        <Text> Hello {this.state.myWorld}</Text>
       </View>
-    )
+    );
   }
-}; 
-//增加type
-Hello.propTypes = {
-  title : PropTypes.string ,  //字串
-  name : PropTypes.string , //字串
-  id : PropTypes.number.isRequired // 數字 & 必填
-};
-//給預設值
-Hello.defaultProps = {
-  title: 'React Native',
-  name: 'Gavin',
-  id: 2022
 }
 
-export default function App() {
-
-  return (
-    <View style ={styles.container}>
-      <Hello title='World' name="User" id={20220708}/> 
-      <Hello />
-    </View>
-  );
-}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
