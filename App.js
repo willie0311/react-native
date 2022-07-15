569b6a7c0d9b2decbf9feeca7c44a67d8eb85a6b
willie <willie@gmail.com>
2022-07-15 10:06:08 +0800

    CSS樣式-FLEX

--- App.js
+++ App.js
@@ -1,62 +1,68 @@
-import { StyleSheet, Text, View,SafeAreaView,TextInput,Button,TouchableHighlight,TouchableOpacity,TouchableNativeFeedback,Image, ScrollView , FlatList ,SectionList ,Switch, Modal,Alert, Dimensions } from 'react-native';
+
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, TouchableHighlight, TouchableOpacity,TouchableNativeFeedback,Image,ScrollView,FlatList,SectionList,Switch,Modal, Dimensions,Animated,Clipboard,} from 'react-native';
+
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
-let {height, width} = Dimensions.get('window');
+{/*let {height, width} = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
         <Image
           style={{height: height * 0.2, width: width * 0.5}}
-          source={require('./assets/123.jpg')}
+          source={require('./ada.jpg')}
         />
     </View>
   );
-}
-
-
-{/*export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      show : false,
-    };
-  }
-
-  render (){
-    return (
-      <View style = {styles.container}>
-        <Button
-          title='Alert'
-          onPress={() => {
-            Alert.alert(
-              'title',
-              'Hello React Native Alert',
-              [
-                { text : 'Cancel' , onPress : () => console.log('Cancel')},
-                { text : 'Yes' , onPress : () => console.log('Yes')},
-                { text : 'No' , onPress : () => console.log('No')},
-              ]
-            )
-          }} 
-        />
-      </View>
-    );
-  }
 }*/}
+
+
+export default class App extends Component{
+  
+
+  render(){
+      return (
+        <View style={styles.container}>
+          <View style={styles.view}>
+            <Text style={{fontSize: 30}}>1</Text>
+          </View>
+          <View style={styles.view}>
+            <Text style={{fontSize: 30}}>2</Text>
+          </View>
+          <View style={styles.view}>
+            <Text style={{fontSize: 30}}>3</Text>
+          </View>
+        </View>
+      );
+  }
+  
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'column'
   },
-  view01: {
-    height:100 ,
-    width:100 ,
-    borderWidth:1
+  view: {
+    height: 50,
+    width: 50,
+    margin: 10,
+    backgroundColor: 'gray',
+  },
+  view02: {
+    flex: 3,
+    backgroundColor: 'white',
+  },
+  view03: {
+    flex: 1,
+    backgroundColor: 'black',
   },
   text01: {
-    color: 'red',
-  }
+    color: '#3f8',
+    textAlign: 'center',
+    fontSize: 20,
+  },
 });
